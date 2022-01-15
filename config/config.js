f491fe3497d8bd4e0cd80667d43b68c6635d4dbe
clarencetw <mr.lin.clarence@gmail.com>
2022-01-15 10:39:25 +0000

    feat: add config setting

---
+++ config/config.js
@@ -0,0 +1,11 @@
+const _ = require('lodash');
+
+const config = path.join(__dirname, 'config.json');
+const defaultConfig = config.development;
+const environment = process.env.NODE_ENV || 'development';
+const environmentConfig = config[environment];
+const finalConfig = _.merge(defaultConfig, environmentConfig);
+
+global.gConfig = finalConfig;
+
+console.log(`global.gConfig: ${JSON.stringify(global.gConfig, undefined, global.gConfig.json_indentation)}`);
